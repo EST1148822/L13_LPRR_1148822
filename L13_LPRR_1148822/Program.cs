fd229639c518fd8e98e560f875a9e6f7e6fb896b
claud <claud@DESKTOP-4A88996>
2022-10-21 13:00:27 -0600

    Add project files.

---
+++ L13_LPRR_1148822/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    class program
+{
+    static void Main()
+    {
+        string[] nombres = new string[5];
+        int[] edad = new int[5];
+        
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Ingrese nombre: ");
+            nombres[i] = Console.ReadLine();
+            Console.WriteLine("Ingrese edad: ");
+            edad[i] = Convert.ToInt32(Console.ReadLine()); 
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (edad[i] >= 18)
+            {
+                Console.WriteLine(nombres[i]);
+            }
+        }
+
+    }
+}
